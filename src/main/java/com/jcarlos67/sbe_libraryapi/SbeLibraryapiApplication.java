6381aeab6d2c15ad68125ad18199ec34a5c2480f
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-08-12 18:26:45 -0300

    first commit

---
+++ src/main/java/com/jcarlos67/sbe_libraryapi/SbeLibraryapiApplication.java
@@ -0,0 +1,13 @@
+package com.jcarlos67.sbe_libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbeLibraryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbeLibraryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcarlos67/sbe_libraryapi/SbeLibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarlos67.sbe_libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbeLibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
