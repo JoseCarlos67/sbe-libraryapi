0b9a7064b242e4f9613e286f35bf51a69b384fba
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-09-11 18:07:50 -0300

    docs: added initial configuration for swagger

---
+++ src/main/java/com/jcarlos67/sbe_libraryapi/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,34 @@
+package com.jcarlos67.sbe_libraryapi.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+  @Bean
+  public OpenAPI defineOpenApi() {
+    Server server = new Server();
+    server.setUrl("http://localhost:8080");
+    server.setDescription("Development");
+
+    Contact myContact = new Contact();
+    myContact.setName("José Carlos Esteves");
+    myContact.setEmail("josecarlos.silesteves@gmail.com");
+
+    Info information = new Info()
+            .title("Library Management System API")
+            .version("1.0")
+            .description("This PAI exposes endpoints for library management.")
+            .contact(myContact);
+
+    return new OpenAPI().info(information).servers(List.of(server));
+  }
+
+}
